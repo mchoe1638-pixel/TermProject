d30cf5ad2f8f7a193a9399599a268965f083fd30
CMK <mchoe1638@gmail.com>
2025-12-04 11:48:43 +0900

    test

--- TermProjectServer/TermProjectServer/server.c
+++ TermProjectServer/TermProjectServer/server.c
@@ -216,42 +216,6 @@ unsigned __stdcall ClientThreadProc(void* arg)
 
     return 0;
 }
-//{
-//    ClientSession* sess = (ClientSession*)arg;
-//    SOCKET cs = sess->sock;
-//
-//    printf("Client session started.\n");
-//
-//    while (g_running && sess->running) {
-//        // 1) 이 세션의 입력만 처리
-//        if (!PollClientInput(sess)) {
-//            printf("Client disconnected (input).\n");
-//            break;
-//        }
-//
-//        // 2) 세션 상태 스냅샷
-//        GameState snapshot;
-//        EnterCriticalSection(&sess->cs);
-//        snapshot = sess->state;
-//        LeaveCriticalSection(&sess->cs);
-//
-//        // 3) 이 세션의 상태만 전송
-//        if (!SendGameState(cs, &snapshot)) {
-//            printf("Client disconnected (send).\n");
-//            break;
-//        }
-//
-//        Sleep(1000 / SERVER_TICK_RATE);
-//    }
-//
-//    sess->running = 0;
-//    closesocket(cs);
-//
-//    DeleteCriticalSection(&sess->cs);
-//    free(sess);
-//
-//    return 0;
-//}
 
 int main(void)
 {
